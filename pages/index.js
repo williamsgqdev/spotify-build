80af3766fb045214aaf9d71507a8a61f422b4481
williamsgqdev <williamsagbunu@gmail.com>
2022-01-11 17:08:17 +0100

    feature : homepage header

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 // import Head from 'next/head'
 import Sidebar from "../components/Sidebar";
+import Center from "../components/Center";
 export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden">
-      <main>
+      <main className="flex">
         <Sidebar />
+        <Center />
       </main>
 
       <div>{/* player */}</div>
